aa82b75a1e7955968c1b8f3c9fc46a8e5961747b
beggsfinest00 <81796822+beggsfinest00@users.noreply.github.com>
2023-02-28 13:17:37 -0600

    Initial commit

---
+++ Casting_Dallas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Casting_Dallas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a whole number here pls");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"the number you pick was {userNumber}");
+        
+            Console.WriteLine("Do you like video games? True or False.");
+            bool likeGames = Convert.ToBoolean(Console.ReadLine());
+            if (likeGames)
+            {
+                Console.WriteLine("So you do like games good!");
+            }             
+            else    
+            {
+                Console.WriteLine("WoW cant beleve you dont like games what are you doing here!");
+            }                  
+               
+             
+
+        }
+    }
+}
